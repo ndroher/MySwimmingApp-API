9161946c4b4aedc9f6bf08355033975cf1f931b4
Leandro H <ndroher+git@proton.me>
2024-10-11 20:58:37 -0300

    feat: add custom post type treinos

---
+++ custom-post-types/treinos_cpt.php
@@ -0,0 +1,72 @@
+<?php
+// Registro do CPT de Treinos
+function register_treinos_cpt() {
+    $labels = array(
+        'name' => 'Treinos',
+        'singular_name' => 'Treino',
+        'add_new' => 'Adicionar Novo',
+        'add_new_item' => 'Adicionar Novo Treino',
+        'edit_item' => 'Editar Treino',
+        'new_item' => 'Novo Treino',
+        'view_item' => 'Ver Treino',
+        'search_items' => 'Pesquisar Treinos',
+        'not_found' => 'Nenhum treino encontrado',
+        'not_found_in_trash' => 'Nenhum treino encontrado na lixeira'
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'custom-fields'),
+        'menu_position' => 5,
+        'rewrite' => array('slug' => 'treinos'),
+    );
+
+    register_post_type('treinos', $args);
+}
+
+add_action('init', 'register_treinos_cpt');
+
+//CUSTOM FIELDS
+function add_treinos_custom_fields() {
+    register_post_meta('treinos', 'tamanho_da_piscina', array(
+        'show_in_rest' => true,
+        'type' => 'integer',
+        'single' => true,
+    ));
+
+    register_post_meta('treinos', 'distancia_total', array(
+        'show_in_rest' => true,
+        'type' => 'integer',
+        'single' => true,
+    ));
+
+    register_post_meta('treinos', 'repeticoes_por_tipo_de_nado', array(
+        'show_in_rest' => true,
+        'type' => 'object',
+        'single' => true,
+    ));
+
+    register_post_meta('treinos', 'equipamentos_utilizados', array(
+        'show_in_rest' => true,
+        'type' => 'object',
+        'single' => true,
+    ));
+
+    register_post_meta('treinos', 'exercicio_id', array(
+        'show_in_rest' => true,
+        'type' => 'integer',
+        'single' => true,
+    ));
+
+    register_post_meta('treinos', 'repeticoes', array(
+        'show_in_rest' => true,
+        'type' => 'integer',
+        'single' => true,
+    ));
+}
+
+add_action('init', 'add_treinos_custom_fields');
+?>

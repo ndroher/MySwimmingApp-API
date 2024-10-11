0606259d19f7fc0f50a069180afe3469d87e681c
Leandro H <ndroher+git@proton.me>
2024-10-11 20:58:05 -0300

    feat: add endpoints cpt exercicios

---
+++ endpoints/exercicio/exercicio_get.php
@@ -0,0 +1,63 @@
+<?php
+function api_exercicio_get($request) {
+    $user = wp_get_current_user();
+    $user_id = $user->ID;
+
+    if ($user_id === 0) {
+        return new WP_Error('error', 'Usuário não autorizado', ['status' => 401]);
+    }
+
+    $exercicio_id = $request['id'];
+
+    if ($exercicio_id) {
+        // Recupera um exercício específico
+        $exercicio = get_post($exercicio_id);
+
+        if (!$exercicio || $exercicio->post_type !== 'exercicios' || $exercicio->post_author != $user_id) {
+            return new WP_Error('error', 'Exercício não encontrado', ['status' => 404]);
+        }
+
+        $response = array(
+            'id' => $exercicio->ID,
+            'nome' => $exercicio->post_title,
+            'tipo_nado' => wp_get_post_terms($exercicio->ID, 'tipo_nado', array("fields" => "names")),
+            'equipamentos' => wp_get_post_terms($exercicio->ID, 'equipamentos', array("fields" => "names")),
+            'personalizado' => get_post_meta($exercicio->ID, 'personalizado', true),
+        );
+    } else {
+        // Recupera todos os exercícios do usuário
+        $exercicios = get_posts(array(
+            'post_type' => 'exercicios',
+            'author' => $user_id,
+            'posts_per_page' => -1
+        ));
+
+        $response = array();
+        foreach ($exercicios as $exercicio) {
+            $response[] = array(
+                'id' => $exercicio->ID,
+                'nome' => $exercicio->post_title,
+                'tipo_nado' => wp_get_post_terms($exercicio->ID, 'tipo_nado', array("fields" => "names")),
+                'equipamentos' => wp_get_post_terms($exercicio->ID, 'equipamentos', array("fields" => "names")),
+                'personalizado' => get_post_meta($exercicio->ID, 'personalizado', true),
+            );
+        }
+    }
+
+    return rest_ensure_response($response);
+}
+
+function register_api_exercicio_get() {
+    register_rest_route('api', '/exercicio/(?P<id>\d+)', array(
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => 'api_exercicio_get',
+    ));
+
+    register_rest_route('api', '/exercicios', array(
+        'methods' => WP_REST_Server::READABLE,
+        'callback' => 'api_exercicio_get',
+    ));
+}
+
+add_action('rest_api_init', 'register_api_exercicio_get');
+?>
---
+++ endpoints/exercicio/exercicio_put.php
@@ -0,0 +1,50 @@
+<?php
+function api_exercicio_put($request) {
+    $user = wp_get_current_user();
+    $user_id = $user->ID;
+
+    if ($user_id === 0) {
+        return new WP_Error('error', 'Usuário não autorizado', ['status' => 401]);
+    }
+
+    $exercicio_id = $request['id'];
+    $exercicio = get_post($exercicio_id);
+
+    if (!$exercicio || $exercicio->post_type !== 'exercicios' || $exercicio->post_author != $user_id) {
+        return new WP_Error('error', 'Exercício não encontrado', ['status' => 404]);
+    }
+
+    $nome = sanitize_text_field($request['nome']);
+    $tipo_nado = sanitize_text_field($request['tipo_nado']);
+    $equipamentos = sanitize_text_field($request['equipamentos']);
+    $personalizado = sanitize_text_field($request['personalizado']);
+
+    wp_update_post(array(
+        'ID' => $exercicio_id,
+        'post_title' => $nome,
+    ));
+
+    wp_set_object_terms($exercicio_id, $tipo_nado, 'tipo_nado');
+    wp_set_object_terms($exercicio_id, $equipamentos, 'equipamentos');
+    update_post_meta($exercicio_id, 'personalizado', $personalizado);
+
+    $response = array(
+        'id' => $exercicio_id,
+        'nome' => $nome,
+        'tipo_nado' => $tipo_nado,
+        'equipamentos' => $equipamentos,
+        'personalizado' => $personalizado,
+    );
+
+    return rest_ensure_response($response);
+}
+
+function register_api_exercicio_put() {
+    register_rest_route('api', '/exercicio/(?P<id>\d+)', array(
+        'methods' => WP_REST_Server::EDITABLE,
+        'callback' => 'api_exercicio_put',
+    ));
+}
+
+add_action('rest_api_init', 'register_api_exercicio_put');
+?>

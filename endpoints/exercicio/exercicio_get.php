b48cd6c02aa11ba10fba55e079f5af50b0ebdd1d
Leandro H <ndroher+git@proton.me>
2024-11-04 00:23:08 -0300

    refactor: update get exercicio

--- endpoints/exercicio/exercicio_get.php
+++ endpoints/exercicio/exercicio_get.php
@@ -1,19 +1,12 @@
 <?php
 function api_exercicio_get($request) {
-    $user = wp_get_current_user();
-    $user_id = $user->ID;
-
-    if ($user_id === 0) {
-        return new WP_Error('error', 'Usuário não autorizado', ['status' => 401]);
-    }
-
     $exercicio_id = $request['id'];
 
     if ($exercicio_id) {
         // Recupera um exercício específico
         $exercicio = get_post($exercicio_id);
 
-        if (!$exercicio || $exercicio->post_type !== 'exercicios' || $exercicio->post_author != $user_id) {
+        if (!$exercicio || $exercicio->post_type !== 'exercicios') {
             return new WP_Error('error', 'Exercício não encontrado', ['status' => 404]);
         }
 
